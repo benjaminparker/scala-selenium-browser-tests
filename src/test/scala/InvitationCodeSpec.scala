b9336d6da67649398421a81e874560f37a9ff946
Benjamin Parker <benjamin@needstyping.com>
2015-11-06 14:51:38 +0000

    Making test more domain driven

--- src/test/scala/InvitationCodeSpec.scala
+++ src/test/scala/InvitationCodeSpec.scala
@@ -4,29 +4,28 @@ import org.specs2.mutable._
 import org.specs2.specification.Scope
 
 class InvitationCodeSpec extends Specification with Scope {
-  val invitationCodeCheck = "https://ppd-ges.notprod.homeoffice.gov.uk/register-to-apply/invitationCodeCheck"
+  val invitationCodeCheck = "http://localhost:3022/register-to-apply/invitationCodeCheck"
 
-  trait Context extends Scope {
-    val driver = new HtmlUnitDriver()
-    val browser = new Fluent(driver) {}
+  trait InvitationCodeRedemptionPage extends Scope {
+    val browser = new Fluent(new HtmlUnitDriver()) {}
     browser.getCookies.clear
     browser.goTo(invitationCodeCheck)
   }
 
   "Redeem Invitation Code" should {
 
-    "show an error when no code is entered" in new Context {
+    "show an error when NO code is entered" in new InvitationCodeRedemptionPage {
       browser.click("#updateContinue")
       browser.url must endWith("/invitationCodeError")
     }
 
-    "show an error when an invalid code is entered" in new Context {
+    "show an error when an INVALID code is entered" in new InvitationCodeRedemptionPage {
       browser.fill("#invitationCode").`with`("NV4L1DC0D3")
       browser.submit("#updateContinue")
       browser.url must endWith("/invitationCodeError")
     }
 
-    "go to start of form when an valid code is entered" in new Context {
+    "go to start of form when a VALID code is entered" in new InvitationCodeRedemptionPage {
       browser.fill("#invitationCode").`with`("SMOKETOKEN")
       browser.submit("#updateContinue")
       browser.url must endWith("/register-to-apply/registration")
