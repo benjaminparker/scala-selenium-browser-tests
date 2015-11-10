73d20049e92e991c695687f28ba68b1df70bcc1e
Benjamin Parker <benjamin@needstyping.com>
2015-11-10 11:56:20 +0000

    More lovely language - to make it more accessible

--- src/test/scala/fixtures/InvitationCodePage.scala
+++ src/test/scala/fixtures/InvitationCodePage.scala
@@ -4,16 +4,20 @@ import org.fluentlenium.core.Fluent
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.specs2.matcher.MatchResult
 import org.specs2.mutable._
-import org.specs2.specification.Scope
+import org.specs2.specification.{AfterEach, Scope}
 
 
-trait InvitationCodePage extends SpecLike with Scope {
-  val page = new Fluent(new HtmlUnitDriver()) {}
+trait InvitationCodePage extends SpecLike with Scope with AfterEach {
+  private val browser = new HtmlUnitDriver()
+  val page = new Fluent(browser) {}
   page.getCookies.clear
-  page.goTo("http://localhost:3022/register-to-apply/invitationCodeCheck")
 
-  def invitationCode: String => Fluent = code => page.fill("#invitationCode").`with`(code)
-  def submit = page.click("#updateContinue")
-  def beErrorPageURL: String => MatchResult[String] = url => url must endWith("/invitationCodeError")
-  def beFormStartPageURL: String => MatchResult[String] = url => url must endWith("/register-to-apply/registration")
+  def goToInvitationCodePage = page.goTo("http://localhost:3022/register-to-apply/invitationCodeCheck")
+  def typeInvitationCode: String => Fluent = code => page.fill("#invitationCode").`with`(code)
+  def submitInvitationCode = page.click("#updateContinue")
+  def currentPage() = page.url
+  def beTheErrorPage(): String => MatchResult[String] = url => url must endWith("/invitationCodeError")
+  def beFormStartPage(): String => MatchResult[String] = url => url must endWith("/register-to-apply/registration")
+
+  override def after = browser.close()
 }
--- src/test/scala/invitationcode/InvitationCodeSpec.scala
+++ src/test/scala/invitationcode/InvitationCodeSpec.scala
@@ -9,20 +9,23 @@ class InvitationCodeSpec extends Specification with Scope {
   "Redeem Invitation Code" should {
 
     "show an error when NO code is entered" in new InvitationCodePage {
-      submit
-      page.url must beErrorPageURL
+      goToInvitationCodePage
+      submitInvitationCode
+      currentPage must beTheErrorPage
     }
 
     "show an error when an INVALID code is entered" in new InvitationCodePage {
-      invitationCode("NV4L1DC0D3")
-      submit
-      page.url must beErrorPageURL
+      goToInvitationCodePage
+      typeInvitationCode("NV4L1DC0D3")
+      submitInvitationCode
+      currentPage must beTheErrorPage
     }
 
     "go to start of form when a VALID code is entered" in new InvitationCodePage {
-      invitationCode("SMOKETOKEN")
-      submit
-      page.url must beFormStartPageURL
+      goToInvitationCodePage
+      typeInvitationCode("SMOKETOKEN")
+      submitInvitationCode
+      currentPage must beFormStartPage
     }
   }
 }
